88b6a57518ec89fab9d3e353c949a3833b7c9741
Aayush Pachpande <aayushpachpande@gmail.com>
2023-08-26 23:27:24 +0530

    commit

--- blogfrontend/src/App.js
+++ blogfrontend/src/App.js
@@ -8,12 +8,12 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-<<<<<<< HEAD
+
 import Demo from './pages/Demo';
-=======
+
 import TrekAPIService from './pages/TrekAPIService/TrekAPIService';
 import Signup from './pages/Signup';
->>>>>>> 3fd4478bce6a0193fbfa8a6922320520c7fa934a
+
 function App() {
   //this is a comment
   return (
@@ -21,12 +21,8 @@ function App() {
    <BrowserRouter>
       <Base/>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Demo/>} />
-=======
-      
         <Route path="/" element={<Home/>} />
->>>>>>> 3fd4478bce6a0193fbfa8a6922320520c7fa934a
         <Route path="Login" element={<Login/>} />
         <Route path="Signup" element={<Signup/>}/>
         <Route path="About" element={<About/>}/>
